e4d978fceabf840b8193d55456ff560765728bfe
Mil4n <sijanmilan@gmail.com>
2013-07-13 01:22:25 +0200

    Reading from the index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
+var fs = require('fs');
 var express = require('express');
+
+var INDEXDEFAULT_FILENAME = "index.html";
+
+var getBufferedIndex = function() {
+    return fs.readFileSync(INDEXDEFAULT_FILENAME);
+}
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(getBufferedIndex().toString());
 });
 
 var port = process.env.PORT || 5000;
